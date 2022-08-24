e7c969fb1306afb8bd234c887f7489720d8b00bf
Mauricio P Guidini <mauricio_guidini@msn.com>
2022-08-24 17:43:08 -0300

    first eureka

---
+++ src/test/java/comgep/sigpesng/eurekaserver/SgpngEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package comgep.sigpesng.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpngEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
